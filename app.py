72c27d479430f3c6ef48288e2e2cb2571d0f553a
USAGroup4 <progamtesting@gmail.com>
2023-01-12 19:20:19 +0800

    Project ready for deployment

--- app.py
+++ app.py
@@ -1,36 +1,33 @@
 import os
-import tensorflow as tf
-import numpy as np
 
-from PIL import Image
 import cv2
-from keras.models import load_model
+import numpy as np
+from PIL import Image
 from flask import Flask, request, render_template
+from keras.models import load_model
 from werkzeug.utils import secure_filename
-
 
 app = Flask(__name__)
 
 
-model =load_model('BrainTumor150Epochs.h5')
+model = load_model('BrainTumor150Epochs.h5')
 print('Model loaded. Check http://127.0.0.1:5000/')
 
 
 def get_className(classNo):
-	if classNo==0:
-		return "No Brain Tumor \U0001f600"
-	elif classNo==1:
-		return "There's a Brain Tumor! \t\n Go to Hospital for more information!"
+    if classNo == 0:
+        return "No Brain Tumor \U0001f600"
+    elif classNo == 1:
+        return "There's a Brain Tumor! \t\n Go to Hospital for more information!"
 
 
 def getResult(img):
-    image=cv2.imread(img)
+    image = cv2.imread(img)
     image = Image.fromarray(image, 'RGB')
     image = image.resize((64, 64))
-    image=np.array(image)
+    image = np.array(image)
     input_img = np.expand_dims(image, axis=0)
-    result=model.predict(input_img)
-    #result = np.argmax(result,axis=1)
+    result = model.predict(input_img)
     return result
 
 
@@ -48,11 +45,11 @@ def upload():
         file_path = os.path.join(
             basepath, 'uploads', secure_filename(f.filename))
         f.save(file_path)
-        value=getResult(file_path)
-        result=get_className(value) 
+        value = getResult(file_path)
+        result = get_className(value)
         return result
     return None     
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0",port=5000)
+    app.run(debug=True, host="0.0.0.0", port=5000)
